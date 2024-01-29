da657a76bbe66924e0c4bd01e80dabcf0256c8b5
Moi <oxspit@gmail.com>
2024-01-29 01:14:14 +0100

    new message auto scroll and notif

--- ui/src/app/routes/room/components/game-chat/game-chat.component.ts
+++ ui/src/app/routes/room/components/game-chat/game-chat.component.ts
@@ -1,5 +1,5 @@
 import { DatePipe, NgClass, NgFor, NgIf, NgSwitch, NgSwitchCase } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Signal, computed } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, Signal, ViewChild, computed, effect } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { SendButtonComponent } from 'src/app/components/send-button/send-button.component';
 import { RoomChat, RoomWsService, Turn } from 'src/app/services/websocket/room.wsService';
@@ -7,6 +7,8 @@ import { UserMessageComponent } from './messages/user-message.component';
 import { GameMessage, MessageType, PositionedUser } from './models';
 import { ServerMessageComponent } from './messages/server-message.component';
 import { TurnMessageComponent } from './messages/turn-message.component';
+import { UserService } from 'src/app/services';
+import { take, timer } from 'rxjs';
 
 const DEPENDENCIES = [NgIf, NgFor, NgClass, NgSwitch, NgSwitchCase, DatePipe, ReactiveFormsModule, SendButtonComponent, UserMessageComponent, ServerMessageComponent, TurnMessageComponent];
 const SERVER_AUTHOR = '_ROOM_';
@@ -30,15 +32,28 @@ const DCD_USER: PositionedUser = {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GameChatComponent {
+  @ViewChild('chatBody') chatBody!: ElementRef;
+
   MessageType = MessageType;
   colors = ['#A9DEF950','#fd545450','#BFD2BF50','#D3F8E250','#E4C1F950'];
   messages: Signal<GameMessage[]> = computed(() => this.updateRoomMessages());
+  showScrollBotButton = false;
   
   chatControl = new FormControl('');
 
   messageFn = (_: number, message: GameMessage) => message.sent_at;
 
-  constructor(private roomService: RoomWsService) {}
+  constructor(private roomService: RoomWsService, private userService: UserService) {
+    effect(() => {
+      const lastMessageAuthor = this.messages()[0]?.author;
+
+      if (lastMessageAuthor?.id === this.userService.user?.id) {
+        this.scrollToBottom();
+      } else if (this.chatBody?.nativeElement?.scrollTop < 0) {
+        this.showScrollBotButton = true;
+      }
+    });
+  }
 
   sendMessage(): void {
     if (this.chatControl.value) {
@@ -56,6 +71,15 @@ export class GameChatComponent {
     const position: 'left' | 'right' = author_position % 2 == 0 ? 'left' : 'right';
     const color = this.colors[author_position % this.colors.length];
     return { ...this.roomService.room().users[author_position], position, color };
+  }
+
+  scrollToBottom(): void {
+    this.showScrollBotButton = false;
+    timer(100).pipe(take(1)).subscribe({
+      next: () => {
+        this.chatBody?.nativeElement?.scrollTo({ top: 1, left: 0, behavior: 'smooth' });
+      },
+    });
   }
 
   private updateRoomMessages(): GameMessage[] {
