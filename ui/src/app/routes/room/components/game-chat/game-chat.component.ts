16bbd9f9f0197aaae1f1793fab6ba254ce41403f
Moi <oxspit@gmail.com>
2024-02-03 21:56:11 +0100

    filters for game chat now

--- ui/src/app/routes/room/components/game-chat/game-chat.component.ts
+++ ui/src/app/routes/room/components/game-chat/game-chat.component.ts
@@ -1,5 +1,5 @@
 import { DatePipe, NgClass, NgFor, NgIf, NgSwitch, NgSwitchCase } from '@angular/common';
-import { ChangeDetectionStrategy, Component, ElementRef, Signal, ViewChild, computed, effect } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, Signal, ViewChild, computed, effect, signal } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { SendButtonComponent } from 'src/app/components/send-button/send-button.component';
 import { RoomChat, RoomWsService, Turn } from 'src/app/services/websocket/room.wsService';
@@ -10,7 +10,7 @@ import { TurnMessageComponent } from './messages/turn-message.component';
 import { UserService } from 'src/app/services';
 import { take, timer } from 'rxjs';
 
-const DEPENDENCIES = [NgIf, NgFor, NgClass, NgSwitch, NgSwitchCase, DatePipe, ReactiveFormsModule, SendButtonComponent, UserMessageComponent, ServerMessageComponent, TurnMessageComponent];
+const IMPORTS = [NgIf, NgFor, NgClass, NgSwitch, NgSwitchCase, DatePipe, ReactiveFormsModule, SendButtonComponent, UserMessageComponent, ServerMessageComponent, TurnMessageComponent];
 const SERVER_AUTHOR = '_ROOM_';
 const DCD_USER: PositionedUser = {
   id: 'dcd',
@@ -20,9 +20,16 @@ const DCD_USER: PositionedUser = {
   position: 'left',
 };
 
+type Filters = {
+  player1: boolean;
+  player2: boolean;
+  serverChat: boolean;
+  roomChat: boolean;
+};
+
 @Component({
   standalone: true,
-  imports: DEPENDENCIES,
+  imports: IMPORTS,
   selector: 'game-chat',
   templateUrl: './game-chat.component.html',
   styleUrls: [
@@ -36,6 +43,17 @@ export class GameChatComponent {
 
   MessageType = MessageType;
   colors = ['#A9DEF950','#fd545450','#BFD2BF50','#D3F8E250','#E4C1F950'];
+  filters = signal<Filters>({ player1: true, player2: true, roomChat: true, serverChat: true });
+  filterValues = computed<{ key: keyof Filters, value: string }[]>(() => {
+    const [player1, player2] = this.roomService.room().game.players;
+
+    return [
+      { key: 'player1', value: player1.username },
+      { key: 'roomChat', value: 'Room chat' },
+      { key: 'serverChat', value: 'Server messages' },
+      { key: 'player2', value: player2.username },
+    ];
+  });
   messages: Signal<GameMessage[]> = computed(() => this.updateRoomMessages());
   showScrollBotButton = false;
   
@@ -73,28 +91,44 @@ export class GameChatComponent {
     return { ...this.roomService.room().users[author_position], position, color };
   }
 
-  scrollToBottom(): void {
+  scrollToBottom(behavior: 'smooth' | 'instant' = 'smooth'): void {
     this.showScrollBotButton = false;
     timer(100).pipe(take(1)).subscribe({
       next: () => {
-        this.chatBody?.nativeElement?.scrollTo({ top: 1, left: 0, behavior: 'smooth' });
+        this.chatBody?.nativeElement?.scrollTo({ top: 1, left: 0, behavior });
       },
     });
   }
 
+  toggleFilter(filter: keyof Filters): void {
+    this.scrollToBottom('instant');
+    this.filters.mutate(filters => filters[filter] = !filters[filter]);
+  }
+
   private updateRoomMessages(): GameMessage[] {
-    const { messages, game: { history } } = this.roomService.room();
+    const { messages, game: { history, players } } = this.roomService.room();
+    const { player1, player2, roomChat, serverChat } = this.filters();
+
     type Message = Turn | RoomChat;
-    const ordered = (<Message[]>messages).concat(<Message[]>history);
+    const ordered = (<Message[]>messages)
+      .concat(<Message[]>history)
+      .filter(message => {
+        if ((message as Turn).play) {
+          // if turn, return only those with active filters
+          return message.user_id === players[0].id ? player1 : player2;
+        }
+        // if roomMessage return server or user messages depending on filters.
+        return message.user_id === SERVER_AUTHOR ? serverChat : roomChat;
+      });
     ordered.sort((a, b) => +new Date(a.sent_at) - +new Date(b.sent_at));
 
     return ordered
-      .reduce((acc, message) => {
+      .reduce((gameMessages, message) => {
         const sent_at = +new Date(message.sent_at);
         const author = this.getAuthor(message.user_id);
 
         if ((message as Turn).play) {
-          acc.push({
+          gameMessages.push({
             author,
             sent_at,
             play: (message as Turn).play,
@@ -102,21 +136,19 @@ export class GameChatComponent {
             dead: (message as Turn).result[1],
             type: MessageType.Turn,
           });
-          return acc;
+          return gameMessages;
         }
 
-        const previousMessageMut: GameMessage = acc.at(-1) || {
+        const previousMessageMut: GameMessage = gameMessages.at(-1) || {
           type: message.user_id === SERVER_AUTHOR ? MessageType.Server : MessageType.User,
-          author: this.getAuthor(message.user_id),
           sent_at: 0,
-          message: [],
         };
 
         if (previousMessageMut.type === MessageType.User && previousMessageMut?.author?.id === message.user_id) {
           previousMessageMut!.message!.push((message as RoomChat).message);
           previousMessageMut!.sent_at = +new Date(message.sent_at);
         } else {
-          acc.push({
+          gameMessages.push({
             type: message.user_id === SERVER_AUTHOR ? MessageType.Server : MessageType.User,
             author: this.getAuthor(message.user_id),
             sent_at: +new Date(message.sent_at),
@@ -124,7 +156,7 @@ export class GameChatComponent {
           });
         }
 
-        return acc;
+        return gameMessages;
       }, [] as GameMessage[])
       .reverse();
   }
